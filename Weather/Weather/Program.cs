4390a50e52a49bf7c0071f191bcbeeca69d8dd5c
Fedor Kravchuk <fedor@mail.ru>
2018-04-26 16:57:16 +0300

    XMLWeather

---
+++ Weather/Weather/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace _Weather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Weather weather = new Weather();
+            weather.WeatherDay();
+
+        }
+    }
+}
---
+++ Weather/Weather/Weather.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace _Weather
+{
+    class Weather
+    {
+        XmlTextReader reader = new XmlTextReader("APIWeather.xml");
+        public void WeatherDay()
+        {
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                    if (reader.Name.Equals("day"))
+                    {
+                        string date = reader.GetAttribute("value");
+                        date = date.Insert(4, ".");
+                        date = date.Insert(7, ".");
+                        Console.WriteLine($"{reader.GetAttribute("name")} {date}");
+                    }
+                if (reader.Name.Equals("tempmin"))
+                    Console.WriteLine($"Мин. тепмература:{reader.GetAttribute("value")}{reader.GetAttribute("unit")}");
+                if (reader.Name.Equals("tempmax"))
+                {
+                    Console.WriteLine($"Макс. тепмература:{reader.GetAttribute("value")}{reader.GetAttribute("unit")}");
+                    break;
+                }
+               
+            }
+        }
+        public void WeatherNow()
+        {
+            while (reader.Read())
+            {
+                Console.WriteLine("Погода");
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.Name.Equals("day"))
+                    {
+                        Console.WriteLine();
+                        string date = reader.GetAttribute("value");
+                        date = date.Insert(4, ".");
+                        date = date.Insert(7, ".");
+                        Console.WriteLine($"{reader.GetAttribute("name")} {date}");
+                    }
+                   
+                }
+            }
+        }
+
+    }
+}
